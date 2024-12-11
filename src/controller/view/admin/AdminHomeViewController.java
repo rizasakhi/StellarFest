4afc6f6091ea31f2d60e5b5741d9dc6f463b12ac
Efran Nathanael <37508513+BadAccuracyID@users.noreply.github.com>
2024-12-11 16:15:18 +0700

    feat: properly switch scene

--- src/controller/view/admin/AdminHomeViewController.java
+++ src/controller/view/admin/AdminHomeViewController.java
@@ -1,13 +1,17 @@
 package controller.view.admin;
 
+import view.StageManager;
+import view.admin.EventManagementView;
+import view.admin.UserManagementView;
+
 public class AdminHomeViewController {
 
     public static void handleManageEventRedirect() {
-        //
+        StageManager.getInstance().switchScene(EventManagementView.class);
     }
 
     public static void handleManageUserRedirect() {
-        //
+        StageManager.getInstance().switchScene(UserManagementView.class);
     }
 
 }

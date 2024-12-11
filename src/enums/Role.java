24b26650b8c2390730ca4af2e875291a69a7539e
Efran Nathanael <37508513+BadAccuracyID@users.noreply.github.com>
2024-12-11 13:32:09 +0700

    feat: create role enum

---
+++ src/enums/Role.java
@@ -0,0 +1,27 @@
+package enums;
+
+public enum Role {
+
+    EVENT_ORGANIZER("Event Organizer"),
+    VENDOR("Vendor"),
+    GUEST("Guest");
+
+    private final String role;
+
+    Role(String role) {
+        this.role = role;
+    }
+
+    public static Role getRole(String role) {
+        for (Role r : Role.values()) {
+            if (r.getRole().equals(role)) {
+                return r;
+            }
+        }
+        return null;
+    }
+
+    public String getRole() {
+        return this.role;
+    }
+}

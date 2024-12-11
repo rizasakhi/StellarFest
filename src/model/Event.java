ef1de88b0100b13d78d49e56b3fba49bb95c987b
Efran Nathanael <37508513+BadAccuracyID@users.noreply.github.com>
2024-12-11 14:29:00 +0700

    feat: improve event model

--- src/model/Event.java
+++ src/model/Event.java
@@ -1,11 +1,84 @@
 package model;
 
+import java.time.ZonedDateTime;
+import java.util.Objects;
+
 public class Event {
-	private String event_id;
-	private String event_name;
-	private String event_location;
-	private String event_description;
-	private String organizer_id;
-	
-	
+
+    private final long id;
+    private String name;
+    private ZonedDateTime date;
+    private String location;
+    private String description;
+
+    public Event(long id) {
+        this.id = id;
+    }
+
+    public Event(long id, String name, ZonedDateTime date, String location, String description) {
+        this.id = id;
+        this.name = name;
+        this.date = date;
+        this.location = location;
+        this.description = description;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ZonedDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(ZonedDateTime date) {
+        this.date = date;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Event{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", date=" + date +
+                ", location='" + location + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Event)) return false;
+        Event event = (Event) o;
+        return id == event.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
 }

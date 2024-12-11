4c49ab34080e8d8d7c620a0b19f05aa7c889a4ab
Efran Nathanael <37508513+BadAccuracyID@users.noreply.github.com>
2024-12-11 16:52:17 +0700

    feat: show organizer and attendee details

--- src/view/admin/EventManagementView.java
+++ src/view/admin/EventManagementView.java
@@ -15,10 +15,17 @@ import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import model.Event;
+import model.user.User;
+import model.user.impl.EOUser;
+import model.user.impl.GuestUser;
+import model.user.impl.VendorUser;
 import util.DateUtil;
 import util.StringUtil;
 import view.SFView;
 import view.StageManager;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class EventManagementView extends SFView {
 
@@ -88,7 +95,9 @@ public class EventManagementView extends SFView {
                 createDetailRow("Name:", event.getName()),
                 createDetailRow("Date:", DateUtil.formatDate(event.getDate(), DateUtil.DATE_FORMAT)),
                 createDetailRow("Location:", event.getLocation()),
-                createDetailRow("Description:", event.getDescription())
+                createDetailRow("Description:", event.getDescription()),
+                createOrganizerDetailRow(event.getOrganizer().getValue()),
+                createAttendeeDetailRow(event.getAttendees().getValue())
         );
 
         Scene detailsScene = new Scene(detailsContainer, 400, 300);
@@ -105,6 +114,63 @@ public class EventManagementView extends SFView {
 
         HBox row = new HBox(10);
         row.getChildren().addAll(label, content);
+        return row;
+    }
+
+    private HBox createOrganizerDetailRow(EOUser organizer) {
+        Label label = new Label("Organizer:");
+        label.setStyle("-fx-font-weight: bold; -fx-min-width: 100px;");
+
+        VBox organizerDetails = new VBox(5);
+        organizerDetails.getChildren().addAll(
+                createDetailRow("ID:", String.valueOf(organizer.getId())),
+                createDetailRow("Username:", organizer.getUsername()),
+                createDetailRow("Email:", organizer.getEmail())
+        );
+
+        HBox row = new HBox(10);
+        row.getChildren().addAll(label, organizerDetails);
+        return row;
+    }
+
+    private HBox createAttendeeDetailRow(List<User> attendees) {
+        Label label = new Label("Attendees:");
+        label.setStyle("-fx-font-weight: bold; -fx-min-width: 100px;");
+
+        VBox vendorDetails = new VBox(5);
+        Label vendorLabel = new Label("Vendors:");
+        vendorLabel.setStyle("-fx-font-weight: bold; -fx-min-width: 100px;");
+
+        List<User> vendors = attendees.stream()
+                .filter(user -> user instanceof VendorUser)
+                .collect(Collectors.toList());
+
+        for (User vendor : vendors) {
+            vendorDetails.getChildren().addAll(
+                    createDetailRow("ID:", String.valueOf(vendor.getId())),
+                    createDetailRow("Username:", vendor.getUsername()),
+                    createDetailRow("Email:", vendor.getEmail())
+            );
+        }
+
+        VBox guestDetails = new VBox(5);
+        Label guestLabel = new Label("Guests:");
+        guestLabel.setStyle("-fx-font-weight: bold; -fx-min-width: 100px;");
+
+        List<User> guests = attendees.stream()
+                .filter(user -> user instanceof GuestUser)
+                .collect(Collectors.toList());
+
+        for (User guest : guests) {
+            guestDetails.getChildren().addAll(
+                    createDetailRow("ID:", String.valueOf(guest.getId())),
+                    createDetailRow("Username:", guest.getUsername()),
+                    createDetailRow("Email:", guest.getEmail())
+            );
+        }
+
+        HBox row = new HBox(10);
+        row.getChildren().addAll(label, vendorDetails, guestDetails);
         return row;
     }
 }

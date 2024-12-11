770dbc86567c9bcf7fbb975d10e61b28faad0937
Efran Nathanael <37508513+BadAccuracyID@users.noreply.github.com>
2024-12-11 13:48:41 +0700

    feat: actually use main class to launch view

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,20 +2,30 @@ package main;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
+import view.StageManager;
+import view.auth.login.LoginView;
+import view.auth.register.RegisterView;
 
 public class Main extends Application {
 
-    public Main() {
-        launch();
-    }
+    private StageManager stageManager;
 
     public static void main(String[] args) {
-        new Main();
+        launch(args);
     }
 
     @Override
     public void start(Stage stage) {
+        this.stageManager = new StageManager(stage);
 
+        this.registerViews();
+
+        this.stageManager.switchScene(LoginView.class);
+    }
+
+    private void registerViews() {
+        this.stageManager.addScene(new LoginView(this.stageManager));
+        this.stageManager.addScene(new RegisterView(this.stageManager));
     }
 
 }
